521ffa7b820a48cddfb9fda0ba71a70b08f138b9
Harpae <nicolevianth@gmail.com>
2024-10-25 00:18:42 +0700

    Change $encryption_password to environment variable

--- api/config.php
+++ api/config.php
@@ -6,6 +6,6 @@ $db_host = $_ENV["DB_HOST"];
 $db_username = $_ENV["DB_USERNAME"];
 $db_password = $_ENV["DB_PASSWORD"];
 $db_database = $_ENV["DB_DATABASE"];
-$encryption_password = "uxrbXO621JkCkbtInkw36wEpoTmRuvp8";
+$encryption_password = $_ENV["ENCRYPTION_PASSWORD"];
 $api_version = 1;
 ?>
