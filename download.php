e29d7d9ab6129a0b72aec142d84c42a7f5f6d7e4
Serena1432 <admin@s1432.org>
2024-12-25 21:22:19 +0700

    Add a new download type to prevent consuming too much RAM

--- download.php
+++ download.php
@@ -8,25 +8,34 @@ if (!get("id") || !get("index")) redirect_to_home();
 
 $game = new Nbhzvn_Game(intval(get("id"))); $index = intval(get("index")) - 1; $link = $game->links[$index];
 if (!$game->id || !$link) redirect_to_home();
-$path = "./uploads/" . $link->path;
-$extension = strtolower(pathinfo($path, PATHINFO_EXTENSION));
-
-header('Pragma: public');
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-header('Cache-Control: private', false);
-header('Content-Type: application/' . $extension);
-
-header('Content-Disposition: attachment; filename="'. $link->name . '";');
-header('Content-Transfer-Encoding: binary');
-header('Content-Length: ' . filesize($path));
-
-if (ob_get_level()) {
-    ob_end_clean();
-}
-
 $game->add_downloads_count();
-readfile($path);
+
+switch (intval($_ENV["DOWNLOAD_TYPE"])) {
+    case 1: {
+        $path = "./uploads/" . $link->path;
+        $extension = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+        header('Pragma: public');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Cache-Control: private', false);
+        header('Content-Type: application/' . $extension);
+        header('Content-Disposition: attachment; filename="'. $link->name . '";');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Length: ' . filesize($path));
+        if (ob_get_level()) {
+            ob_end_clean();
+            ob_clean();
+            flush();
+        }
+        readfile($path);
+        break;
+    }
+    case 2: {
+        $path = "/uploads/" . $link->path;
+        header("Location: " . $path);
+        break;
+    }
+}
 
 exit;
 ?>
