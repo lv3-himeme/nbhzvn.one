95ff6530d4a80dec261f425451f9c9e231ef9bbb
Serena1432 <admin@s1432.org>
2024-12-28 11:49:22 +0700

    Fix a serious bug while adding new game

--- api/games/functions.php
+++ api/games/functions.php
@@ -2,7 +2,7 @@
 function add_game(stdClass $data, bool $pre_approved = false) {
     db_query('INSERT INTO `nbhzvn_games`
         (`timestamp`, `name`, `links`, `image`, `screenshots`, `description`, `engine`, `tags`, `release_year`, `author`, `language`, `translator`, `uploader`, `status`, `views`, `views_today`, `downloads_today`, `updated_date`, `downloads`, `supported_os`, `is_featured`, `approved`)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ', time(), $data->name, $data->links, $data->image, $data->screenshots, $data->description, $data->engine, $data->tags, $data->release_year, $data->author, $data->language, $data->translator, $data->uploader, $data->status, 0, 0, 0, date('Y-m-d'), 0, $data->supported_os, 0, $pre_approved ? 1 : 0);
     return SUCCESS;
 }
