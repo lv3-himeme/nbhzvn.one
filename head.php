da4cb7ed33424b10e3b0ae01db218a16e879a6b9
Serena1432 <admin@s1432.org>
2025-03-01 00:28:41 +0700

    Change the website font

--- head.php
+++ head.php
@@ -31,7 +31,7 @@ $meta_description = $game ? explode("\n", Html2Text::convert($parsedown->text($g
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
 
     <!-- Css Styles -->
