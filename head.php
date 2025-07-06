9f2cc728f4441a4426da7da06b31f1cb3c4130c5
Tora Phan Nam <admin@s1432.org>
2025-07-06 23:52:54 +0700

    Change the fonts for one last time

--- head.php
+++ head.php
@@ -30,9 +30,7 @@ $meta_description = $game ? explode("\n", Html2Text::convert($parsedown->text($g
     <title><?php if (!$title) echo 'Nobihaza Vietnam Collection'; else echo $title; ?></title>
 
     <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Rosario:wght@300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Geologica" rel="stylesheet">
 
     <!-- Css Styles -->
     <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
