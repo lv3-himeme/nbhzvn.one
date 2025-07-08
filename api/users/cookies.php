2d0da661c3a22da7e496747508a501c36d04df61
Tora Phan Nam <admin@s1432.org>
2025-07-08 19:57:17 +0700

    Update cookie handling

--- api/users/cookies.php
+++ api/users/cookies.php
@@ -4,8 +4,8 @@ function get_user_from_cookie() {
     if (!$username || !$login_token) return null;
     $user = new Nbhzvn_User($username);
     if (!$user->id || !$user->check_login_token($login_token)) {
-        setcookie("nbhzvn_username", "", time() - 3600);
-        setcookie("nbhzvn_login_token", "", time() - 3600);
+        setcookie("nbhzvn_username", "", time() - 3600, "/", "." . $_SERVER["HTTP_HOST"]);
+        setcookie("nbhzvn_login_token", "", time() - 3600, "/", "." . $_SERVER["HTTP_HOST"]);
         header("Location: " . $_SERVER["REQUEST_URI"]);
         return null;
     }
