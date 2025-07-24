7329b113896815ebcb7e3a1ee94ce995c69a1523
Tora Phan Nam <admin@s1432.org>
2025-07-24 09:04:07 +0700

    Fix the upload script
    * Change the file path to old one when uploading images
    * Fix the permissions for folders from 775 to 755
    * Output the error information to the response when encountering file uploading errors

--- api/upload.php
+++ api/upload.php
@@ -7,7 +7,7 @@ require __DIR__ . "/users/cookies.php";
 try {
     $folder = "../uploads";
     if (!file_exists($folder)) {
-        if (!mkdir($folder, 0775, true)) api_response(null, "Có lỗi xảy ra, vui lòng thử lại.", 500);
+        if (!mkdir($folder, 0755, true)) api_response(null, "Có lỗi xảy ra, vui lòng thử lại.", 500);
     }
     switch ($_SERVER["REQUEST_METHOD"]) {
         case "POST": {
@@ -15,7 +15,9 @@ try {
             if (post("type") == "chunk") {
                 $extension = strtolower(pathinfo("./uploads/" . basename(post("file_name")), PATHINFO_EXTENSION));
                 $types = ["zip", "rar", "7z", "jpg", "png", "webp", "jpeg", "gz"];
+                $images = ["jpg", "png", "webp", "jpeg"];
                 if (!in_array($extension, $types)) api_response(null, "Định dạng tệp tin không được hỗ trợ.", 400);
+                if (in_array($extension, $images)) $is_image = true;
                 if (!isset($_FILES["chunk"])) api_response(null, "Không có dữ liệu của tệp tin.", 400);
                 $arguments = ["cursor", "chunks", "file_name"];
                 foreach ($arguments as $argument) {
@@ -25,23 +27,23 @@ try {
                 if (!$generated_name) {
                     $generated_name = random_string(64);
                     $path = $folder . "/" . $generated_name;
-                    mkdir($path, 0775, true);
-                    if (!file_exists($path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.", 500);
+                    mkdir($path, 0755, true);
+                    if (!file_exists($path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.\nThông tin lỗi: " . error_get_last()['message'], 500);
                 }
                 $path = $folder . "/" . $generated_name;
                 $cursor = intval(post("cursor")); $chunks = intval(post("chunks"));
                 if ($cursor < $chunks - 1) {
                     $path .= "/" . strval($cursor);
-                    if (!move_uploaded_file($_FILES["chunk"]["tmp_name"], $path) || !chmod($path, 0775) || !file_exists($path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.", 500);
+                    if (!move_uploaded_file($_FILES["chunk"]["tmp_name"], $path) || !chmod($path, 0755) || !file_exists($path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.\nThông tin lỗi: " . error_get_last()['message'], 500);
                     api_response($generated_name, "Đã hoàn thành tải lên " . ($cursor + 1) . "/" . $chunks . ".");
                 }
                 else {
                     $chunk_path = $path . "/" . strval($cursor);
-                    if (!move_uploaded_file($_FILES["chunk"]["tmp_name"], $chunk_path) || !chmod($chunk_path, 0775) || !file_exists($chunk_path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.", 500);
-                    $file_name = post("file_name");
-                    $file_path = $path . "/" . $file_name;
+                    if (!move_uploaded_file($_FILES["chunk"]["tmp_name"], $chunk_path) || !chmod($chunk_path, 0755) || !file_exists($chunk_path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.\nThông tin lỗi: " . error_get_last()['message'], 500);
+                    $file_name = $is_image ? ($generated_name . "." . strtolower(pathinfo(post("file_name"), PATHINFO_EXTENSION))) : post("file_name");
+                    $file_path = $is_image ? ($folder . "/" . $file_name) : ($path . "/" . $file_name);
                     $file = fopen($file_path, "wb");
-                    if (!$file) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại. 4", 500);
+                    if (!$file) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.\nThông tin lỗi: " . error_get_last()['message'], 500);
                     for ($i = 0; $i < $chunks; $i++) {
                         $chunk_path_tmp = $path . "/" . strval($i);
                         $chunk_data_tmp = file_get_contents($chunk_path_tmp);
@@ -49,18 +51,23 @@ try {
                         unlink($chunk_path_tmp);
                     }
                     fclose($file);
-                    api_response($generated_name, "Tải lên thành công.");
+                    api_response($is_image ? $file_name : $generated_name, "Tải lên thành công.");
                 }
             }
             else {
                 if (!isset($_FILES["file"])) api_response(null, "Vui lòng chọn một tệp tin để tải lên.", 400);
                 $extension = strtolower(pathinfo("./uploads/" . basename($_FILES["file"]["name"]), PATHINFO_EXTENSION));
                 $types = ["zip", "rar", "7z", "jpg", "png", "webp", "jpeg", "gz"];
+                $images = ["jpg", "png", "webp", "jpeg"];
                 if (!in_array($extension, $types)) api_response(null, "Định dạng tệp tin không được hỗ trợ.", 400);
+                if (in_array($extension, $images)) $is_image = true;
                 $generated_name = random_string(64);
-                mkdir($folder . "/" . $generated_name, 0775, true);
-                $path = $folder . "/" . $generated_name . "/" . basename($_FILES["file"]["name"]);
-                if (!move_uploaded_file($_FILES["file"]["tmp_name"], $path) || !file_exists($path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.", 500);
+                if (!$is_image) {
+                    mkdir($folder . "/" . $generated_name, 0755, true);
+                    $path = $folder . "/" . $generated_name . "/" . basename($_FILES["file"]["name"]);
+                }
+                else $path = $folder . "/" . $generated_name . "." . $extension;
+                if (!move_uploaded_file($_FILES["file"]["tmp_name"], $path) || !file_exists($path)) api_response(null, "Không thể tải tệp tin lên vào thời gian này, vui lòng thử lại.\nThông tin lỗi: " . error_get_last()['message'], 500);
                 api_response($generated_name, "Tải lên thành công.");
             }
             break;
