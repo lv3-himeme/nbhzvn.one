67928cc730f8267bb84f8cd90fbcc2258207603c
Tora Phan Nam <nicolevianth@gmail.com>
2025-08-02 21:42:53 +0700

    urlencode path name to prevent special characters errors

--- download_beta.php
+++ download_beta.php
@@ -30,7 +30,7 @@ switch (intval($_ENV["DOWNLOAD_TYPE"])) {
         break;
     }
     case 2: {
-        $path = "/uploads/" . $link->path . "/" . $link->name;
+        $path = "/uploads/" . $link->path . "/" . str_replace("%29", ".", urlencode($link->name));
         header("Location: " . $path);
         break;
     }
