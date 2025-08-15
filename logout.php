9a0734a2773b08fb19db1a8cdc1891ce56ae832c
Tora Phan Nam <admin@s1432.org>
2025-08-15 21:02:43 +0700

    Fix logout error

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+require "api/functions.php";
 setcookie("nbhzvn_username", "", time() - 3600, "/", "." . get_root_domain());
 setcookie("nbhzvn_login_token", "", time() - 3600, "/", "." . get_root_domain());
 setcookie("nbhzvn_username", "", time() - 3600);
